e9298f714742000723a630ac23855145579c2e99
donyw027 <doniwicaksono27@gmail.com>
2023-11-02 23:18:22 +0700

    done

--- application/views/templates/dashboard.php
+++ application/views/templates/dashboard.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title><?= $title; ?> | Aplikasi Sistem Anggaran</title>
+    <title><?= $title; ?> | Aplikasi Sistem Informasi Aset</title>
     <link rel="icon" href="<?= base_url('assets/img/xto.ico'); ?>">
 
     <!-- Custom fonts for this template-->
